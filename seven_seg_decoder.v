b06835c09f0eb50ea2cf6252343ca247826f84ba
gabrieltinsley <156216375+gabrieltinsley@users.noreply.github.com>
2024-10-21 17:03:59 -0600

    Update seven_seg_decoder.v

--- seven_seg_decoder.v
+++ seven_seg_decoder.v
@@ -12,13 +12,13 @@ module seven_seg_decoder(
     // according to the table provided in the lab deliverables section
 
     // Recommended you do a simple behavioral implementation:
-    // alwyas @(*) begin
-    //   case (anode)
-    //      'b1110: selected_sig <= A
-    //      'b1101: selected_sig <= B
-    //      'b1011: selected_sig <= C
-    //      'b0111: selected_sig <= D
-    //   endcase
+    always @(*) begin
+      case (anode)
+         'b1110: selected_sig <= A;
+         'b1101: selected_sig <= B;
+         'b1011: selected_sig <= AplusB;
+         'b0111: selected_sig <= AminusB;
+      endcase
 
     // You will also need a very simple decoder that assigns the segs components
     // based on the 4 bit input number to hexidecimal digit
@@ -26,23 +26,23 @@ module seven_seg_decoder(
     // For reference:
     always @(*) begin
         case(selected_sig)
-            //            GFEDCBA
-            0: segs  = 7'b1000000;        
-            1: segs  = 7'b1111001;        
-            2: segs  = 7'b0100100;        
-            3: segs  = 7'b0110000;        
-            4: segs  = 7'b0011001;        
-            5: segs  = 7'b0010010;        
-            6: segs  = 7'b0000010;        
-            7: segs  = 7'b1111000;        
-            8: segs  = 7'b0000000;
-            9: segs  = 7'b0010000;        
-            10: segs = 7'b0001000;        
-            11: segs = 7'b0000011;        
-            12: segs = 7'b1000110;        
-            13: segs = 7'b0100001;        
-            14: segs = 7'b0000110;        
-            15: segs = 7'b0001110;       
+            //                 GFEDCBA
+            4'b0000: segs = 7'b1000000;  // 0
+            4'b0001: segs = 7'b1111001;  // 1
+            4'b0010: segs = 7'b0100100;  // 2
+            4'b0011: segs = 7'b0110000;  // 3
+            4'b0100: segs = 7'b0011001;  // 4
+            4'b0101: segs = 7'b0010010;  // 5
+            4'b0110: segs = 7'b0000010;  // 6
+            4'b0111: segs = 7'b1111000;  // 7
+            4'b1000: segs = 7'b0000000;  // 8
+            4'b1001: segs = 7'b0010000;  // 9
+            4'b1010: segs = 7'b0001000;  // A
+            4'b1011: segs = 7'b0000011;  // B
+            4'b1100: segs = 7'b1000110;  // C
+            4'b1101: segs = 7'b0100001;  // D
+            4'b1110: segs = 7'b0000110;  // E
+            4'b1111: segs = 7'b0001110;  // F       
         endcase
     end
 
