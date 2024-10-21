e5f59e909aa54852419be11f1bf481ea3f19bc06
gabrieltinsley <156216375+gabrieltinsley@users.noreply.github.com>
2024-10-21 16:47:56 -0600

    Update clock_div.v

--- clock_div.v
+++ clock_div.v
@@ -8,6 +8,39 @@ module clock_div
     output reg div_clock
 );
 
+    reg intreset;
+    wire [16:0] intcount;
+
+    counter #(.WIDTH(17)) count(
+        .clock(clock),
+        .reset(intreset),
+        .count(intcount)
+    );
+
+    // Want to create logic that
+    // is synchronous to the rest
+    // and count signals
+    always @(reset, intcount) begin
+        if (reset) begin
+            // If we get reset, pass it through
+            // to the counter
+            // and reset out clock output
+            intreset = 1;
+            div_clock = 0;
+        end else if (intcount == 100000) begin
+            // Otherwise, if the count is equal
+            // to our tickover point, then we
+            // need to reset the counter and
+            // toggle our output
+            intreset = 1;
+            div_clock = ~div_clock;
+        end else begin
+            // Otherwise, just let it count
+            intreset = 0;
+        end
+        
+    end
+
     // 100 MHz input clock Divide by either 2^17 or use a counter based divider
     // to output to div_clock
 
