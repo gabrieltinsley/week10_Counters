b0df476f654c2aad1a09f8ac39252bc897210d01
gabrieltinsley <156216375+gabrieltinsley@users.noreply.github.com>
2024-10-21 09:25:18 -0600

    Update math_block.v

---
+++ math_block.v
@@ -0,0 +1,12 @@
+module math_block(
+    input [3:0] A,
+    input [3:0] B,
+    output reg [3:0] AplusB,
+    output reg [3:0] AminusB
+);
+
+    // This one should be relatively easy.  You can either use your previous
+    // four bit adders and two's compliment converters that you implemented in
+    // previous labs, or set up some behavioral verilog to do the job for you
+
+endmodule
